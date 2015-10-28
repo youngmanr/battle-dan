49f8d1d81427d13dea5df1b00b794e37a99c256b
Robert Youngman <rob_youngman@yahoo.com>
2015-10-28 12:12:34 +0000

    Chpt.24

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,4 +4,12 @@ feature 'Attacking' do
     click_link 'Attack'
     expect(page).to have_content 'Dave attacked Mittens'
   end
+
+  scenario 'reduce Player 2 Hit Points by 10' do
+    sign_in_and_play
+    click_link 'Attack'
+    click_link 'OK'
+    expect(page).not_to have_content 'Mittens: 60HP'
+    expect(page).to have_content 'Mittens: 50HP'
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,10 +1,31 @@
 require 'player'
 
 describe Player do
-	subject(:dave) {described_class.new('dave')}
+	subject(:dave) {described_class.new('Dave')}
+  subject(:mittens) {described_class.new('Mittens')}
 
+  describe '#name' do
 		it 'returns the name' do
-			expect(dave.name).to eq 'dave'
+			expect(dave.name).to eq 'Dave'
 		end
+  end
 
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(dave.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'damage the player' do
+      expect(mittens).to receive(:receive_damage)
+      dave.attack(mittens)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect {dave.receive_damage}.to change {dave.hit_points}.by(-10)
+    end
+  end
 end
